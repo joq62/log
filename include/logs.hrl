37b78849ef299e69c54f056bb15be0ddf9e33729
joq62 <jakim.leche@gmail.com>
2023-12-19 23:08:31 +0100

    testing log_read

--- include/logs.hrl
+++ include/logs.hrl
@@ -2,3 +2,17 @@
 -define(MaxLogLength,10000).
 -define(LogFile,"central.log").
 -define(LogFilePath,"logs/control.logs").
+
+
+-record(info,{
+	      timestamp,
+	      datetime,
+	      level,
+	      node,
+	      pid,
+	      module,
+	      function,
+	      line,
+	      infotext,
+	      infoargs
+	     }).
--- test/all.erl
+++ test/all.erl
@@ -13,7 +13,7 @@
  
 -export([start/0]).
 
--define(LogFile,"test/logs_short.txt").
+
 %% --------------------------------------------------------------------
 %% Include files
 %% --------------------------------------------------------------------
@@ -26,36 +26,18 @@
 start()->
    
     ok=setup(),
-    ok=read_log_test(),
-    ok=log_test:start(),    
+    ok=log_read_test:start(),
+      
               
     io:format("Test OK !!! ~p~n",[?MODULE]),
-    init:stop(),
-    timer:sleep(2000),
+
+
+%    init:stop(),
+%    timer:sleep(2000),
  
     ok.
 
-%% --------------------------------------------------------------------
-%% Function: available_hosts()
-%% Description: Based on hosts.config file checks which hosts are avaible
-%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
-%% --------------------------------------------------------------------
-read_log_test()->
-    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
-
-    {ok,Bin}=file:read_file(?LogFile),
-    String=erlang:binary_to_list(Bin),
-    NoBreaks=string:lexemes(String,"\n"),
-    [io:format("Str =>> ~p~n",[Str])||Str<-NoBreaks],
-    Lines=[string:lexemes(Str,"|")||Str<-NoBreaks],
-    [io:format(" Line = ~p~n",[Line])||Line<-Lines],
-    
-
-   init:stop(),
-    timer:sleep(2000),
-    
-    ok. 
-    
+   
 %% --------------------------------------------------------------------
 %% Function: available_hosts()
 %% Description: Based on hosts.config file checks which hosts are avaible
